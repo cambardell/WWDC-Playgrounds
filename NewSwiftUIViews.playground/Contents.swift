1c5c190274e8e4e17af3ff10cbed61420925bbad
Cameron Bardell <cambardell3@gmail.com>
2020-06-22 16:36:33 -0400

    Added progress view.

--- NewSwiftUIViews.playground/Contents.swift
+++ NewSwiftUIViews.playground/Contents.swift
@@ -4,20 +4,23 @@ import PlaygroundSupport
 struct ContentView: View {
     var body: some View {
         VStack {
-            TextEditorView()
-            LabelView()
-        }.frame(width: 300, height: 1000)
+            TextEditorViewExample()
+            LabelViewExample()
+            ProgressViewExample()
+            Spacer()
+        }.frame(width: 300)
     }
 }
 
 
 
 
-struct TextEditorView: View {
+struct TextEditorViewExample: View {
     @State private var fullText: String = "This is some editable text..."
 
         var body: some View {
             TextEditor(text: $fullText)
+                .frame(height: 100)
                 .border(Color.black, width: 5)
                 .cornerRadius(8)
                 
@@ -26,13 +29,25 @@ struct TextEditorView: View {
 
 
 
-struct LabelView: View {
+struct LabelViewExample: View {
 
         var body: some View {
             Label("Label", systemImage: "arrow.right")
                 .labelStyle(TitleOnlyLabelStyle())
                 
         }
+}
+
+struct ProgressViewExample: View {
+    
+    @State private var progress = 0.5
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+            Button("More", action: { progress += 0.05 })
+        }
+    }
+    
 }
 
 PlaygroundPage.current.setLiveView(ContentView())
