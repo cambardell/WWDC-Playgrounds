ec6fc3c29bfdc966de8edfdb8c22866bc64e207c
Cameron Bardell <cambardell3@gmail.com>
2020-06-22 16:33:33 -0400

    Added label view.

--- NewSwiftUIViews.playground/Contents.swift
+++ NewSwiftUIViews.playground/Contents.swift
@@ -1,16 +1,39 @@
 import SwiftUI
 import PlaygroundSupport
 
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            TextEditorView()
+            LabelView()
+        }.frame(width: 300, height: 1000)
+    }
+}
+
+
+
+
 struct TextEditorView: View {
     @State private var fullText: String = "This is some editable text..."
 
         var body: some View {
             TextEditor(text: $fullText)
-                .frame(height: 50)
                 .border(Color.black, width: 5)
                 .cornerRadius(8)
                 
         }
 }
 
-PlaygroundPage.current.setLiveView(TextEditorView())
+
+
+struct LabelView: View {
+
+        var body: some View {
+            Label("Label", systemImage: "arrow.right")
+                .labelStyle(TitleOnlyLabelStyle())
+                
+        }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
+
